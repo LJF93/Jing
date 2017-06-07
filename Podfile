4f03e0cc4de8e2733984f60a93202c28217bc1d2
Ljf <675410221@qq.com>
2017-06-07 17:23:13 +0800

    新增AFNetworking实现网络请求的二次封装

--- Podfile
+++ Podfile
@@ -4,6 +4,7 @@ use_frameworks!
 
 target 'Jing' do
 
+    pod 'AFNetworking'
     pod 'IQKeyboardManager'
     pod 'CYLTabBarController'
     pod 'Reachability'
--- Pods/Target Support Files/Pods-Jing/Pods-Jing-frameworks.sh
+++ Pods/Target Support Files/Pods-Jing/Pods-Jing-frameworks.sh
@@ -89,6 +89,7 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CYLTabBarController/CYLTabBarController.framework"
   install_framework "$BUILT_PRODUCTS_DIR/IQKeyboardManager/IQKeyboardManager.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
@@ -96,6 +97,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Reachability/Reachability.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/AFNetworking/AFNetworking.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CYLTabBarController/CYLTabBarController.framework"
   install_framework "$BUILT_PRODUCTS_DIR/IQKeyboardManager/IQKeyboardManager.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MBProgressHUD/MBProgressHUD.framework"
